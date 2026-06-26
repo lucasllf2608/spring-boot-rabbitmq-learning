729c7e909b8815e2e503b9bc3035aa7d3f4478b6
lucasllf2608 <lucas.llf2608@gmail.com>
2026-06-26 11:59:57 -0300

    ajustando o dto para um record

--- src/main/java/com/estudos/rabbitmq/dto/PedidoDTO.java
+++ src/main/java/com/estudos/rabbitmq/dto/PedidoDTO.java
@@ -2,15 +2,6 @@ package com.estudos.rabbitmq.dto;
 
 import java.io.Serializable;
 
-import lombok.Data;
-
-@Data
-public class PedidoDTO implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	private String id;
-	private String descricao;
-	private Double valor;
-	
+public record PedidoDTO(String id, String descricao, Double valor) implements Serializable {
+    private static final long serialVersionUID = 1L;
 }
- 

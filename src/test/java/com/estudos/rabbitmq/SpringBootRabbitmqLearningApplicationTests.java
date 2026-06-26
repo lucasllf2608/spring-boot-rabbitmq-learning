346ce5067d6f2a328a4e9a010f8a59e11fa38fef
lucasllf2608 <lucas.llf2608@gmail.com>
2026-06-26 09:49:04 -0300

    commit inicial

---
+++ src/test/java/com/estudos/rabbitmq/SpringBootRabbitmqLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

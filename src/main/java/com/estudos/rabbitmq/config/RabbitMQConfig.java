cc92019434bdb001db9d4d68eecd4056eb2193c3
lucasllf2608 <lucas.llf2608@gmail.com>
2026-06-26 12:01:09 -0300

    ajustando a configuracao MessageConverter

--- src/main/java/com/estudos/rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/estudos/rabbitmq/config/RabbitMQConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -34,5 +37,22 @@ public class RabbitMQConfig {
 	public MessageConverter jsonMessageConverter() {
 		return new JacksonJsonMessageConverter();
 	}
+	
+	
+	@Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter()); 
+        return factory;
+    }
+	
+	@Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
 	
 }
